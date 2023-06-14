f5d887d23527a54ee6cb30539414171e5a7c6e6a
nilsonweng <nilsonweng@moneydj.com>
2023-06-14 18:45:05 +0800

    [update] 結構完成，樣式仍需調整

--- src/pages/page3.tsx
+++ src/pages/page3.tsx
@@ -1,6 +1,8 @@
 import { NaviGroup } from '@/components/NaviGroup';
 import { NaviItem } from '@/components/NaviItem';
-import React from 'react';
+import React, { useEffect } from 'react';
+
+const isMobile: boolean = false;
 
 export const Page3: React.FC = () => {
     return (
@@ -37,21 +39,100 @@ type Props_Navigator = {
     children: React.ReactNode;
 };
 
-const isMobile = true;
-
 const Navigator: React.FC<Props_Navigator> = ({ children }) => {
-    const modifiedChildren = React.Children.map(children, (child, index) => {
-        // if (index === 0 && React.isValidElement<{ style: React.CSSProperties }>(child)) {
-        //     // 如果是第一個子元件，就把它變成紅色s
-        //     return React.cloneElement(child, { style: { color: 'red' } });
-        // }
+    if (isMobile) {
+        const renderChildren = (children: React.ReactNode, prevLabel = '') => {
+            return React.Children.map(children, (child) => {
+                if (!React.isValidElement(child) || !('type' in child)) return child;
 
-        if (isMobile) {
-        } else {
-        }
+                if (child.type === NaviItem) {
+                    /**
+                     * 只取前一個來產生選單元素
+                     */
+                    if (prevLabel !== '') {
+                        return <li>{prevLabel + child.props.children}</li>;
+                    } else {
+                        return <ul>{child.props.children}</ul>;
+                    }
+                } else if (child.type === NaviGroup) {
+                    const label = child.props.label;
+                    const group = renderChildren(child.props.children, label);
+                    return <ul>{group}</ul>;
+                } else {
+                    console.error('only accepts children of type NaviItem or NaviGroup');
+                }
 
-        return child;
+                return child;
+            });
+        };
+
+        return <div>{renderChildren(children)}</div>;
+    } else {
+        const renderChildren = (children: React.ReactNode, prevLabel = '') => {
+            return React.Children.map(children, (child) => {
+                if (!React.isValidElement(child) || !('type' in child)) return child;
+
+                if (child.type === NaviItem) {
+                    if (prevLabel === '') {
+                        return (
+                            <MyNavGroup className="">
+                                <h1 className="rounded-md border-[1px] border-slate-800 p-2">{child.props.children}</h1>
+                            </MyNavGroup>
+                        );
+                    } else {
+                        return <li>{child.props.children}</li>;
+                    }
+                } else if (child.type === NaviGroup) {
+                    const label = child.props.label;
+                    const group = renderChildren(child.props.children, label);
+                    if (prevLabel === '') {
+                        return (
+                            <MyNavGroup className="">
+                                <h1 className="pointer-events-none rounded-md border-[1px] border-slate-800 p-2">
+                                    {label}
+                                </h1>
+                                <ul className={'mt-1 border-[1px] pl-3'}>{group}</ul>
+                            </MyNavGroup>
+                        );
+                    } else {
+                        return (
+                            <>
+                                <h1>{label}</h1>
+                                <ul className={'mt-1 border-[1px] pl-3'}>{group}</ul>
+                            </>
+                        );
+                    }
+                } else {
+                    console.error('only accepts children of type NaviItem or NaviGroup');
+                }
+
+                return child;
+            });
+        };
+        return <div className="flex gap-4">{renderChildren(children)}</div>;
+    }
+};
+
+const MyNavGroup = ({ className, children }) => {
+    const [open, setOpen] = React.useState(true);
+    const ref = React.useRef<HTMLUListElement | null>(null);
+
+    useEffect(() => {
+        const toggle = () => setOpen((s) => !s);
+        ref.current?.addEventListener('click', toggle);
+        return () => {
+            ref.current?.removeEventListener('click', toggle);
+        };
     });
 
-    return <div>{modifiedChildren}</div>;
+    return (
+        <ul ref={ref} className={className} aria-state={`${open ? 'open' : 'hidden'}`}>
+            {React.Children.map(children, (child, i) => {
+                if (child.type !== 'h1') {
+                    return React.cloneElement(child, { style: { display: `${open ? 'block' : 'none'}` } });
+                }
+                return child;
+            })}
+        </ul>
+    );
 };
